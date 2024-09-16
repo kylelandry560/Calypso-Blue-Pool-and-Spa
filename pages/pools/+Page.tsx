af5fa6891b087db35a4aea4db085dbd72db9e017
Bikash Shrestha <shresthab2@southernct.edu>
2024-09-16 17:17:28 -0400

    test

--- pages/pools/+Page.tsx
+++ pages/pools/+Page.tsx
@@ -5,6 +5,7 @@ export default function Page () {
         <>
             <h3>Pool Section</h3>
            <p>This pool section should be Blog-like and Documentation-Like with a slight tinge of sales tactics, but not overbearingly. Preferably there should be a navigation drawer on the left.</p> 
+           <p> Bikash wrote this</p>
         </>
     )
 }
