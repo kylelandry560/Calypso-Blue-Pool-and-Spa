96faefc96925963f81cdc6302180ed0ff87547cc
Bikash Shrestha <shresthab2@southernct.edu>
2024-09-17 20:30:16 -0400

    Spa Brands

--- pages/spas/+Page.tsx
+++ pages/spas/+Page.tsx
@@ -1,10 +1,63 @@
-import React from 'react'
+// Page.tsx
+import React from "react";
+import Card from "./Card"; // Import the Card component
+import "./spa.css"; // Import the CSS file
 
-export default function Page () {
-    return (
-        <>  
-           <h5>Spa section</h5>
-           <p>This should be Blog-like and Documentation-Like with a slight tinge of sales tactics, but not overbearingly. Preferably there should be a navigation drawer on the left.</p> 
-        </>
-    )
+const data = [
+  {
+    image:
+      "https://images.unsplash.com/photo-1508193638397-1c4234db14d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
+    title: "Best forests to visit in North America",
+    category: "Brand 1",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1559494007-9f5847c49d94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8fHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
+    title: "Hawaii beaches review: better than you think",
+    category: "Brand 2",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1608481337062-4093bf3ed404?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8fHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
+    title: "Mountains at night: 12 best locations to enjoy the view",
+    category: "Brand 3",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1507272931001-fc06c17e4f43?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8fHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
+    title: "Aurora in Norway: when to visit for best experience",
+    category: "Brand 4",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1510798831971-661eb04b3739?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8fHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
+    title: "Best places to visit this winter",
+    category: "Brand 5",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1582721478779-0ae163c05a60?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8fHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=80",
+    title: "Active volcanos reviews: travel at your own risk",
+    category: "Brand 6",
+  },
+];
+
+function Page() {
+  return (
+    <>
+      <h5>Spa section</h5>
+      <p>
+        This should be Blog-like and Documentation-Like with a slight tinge of
+        sales tactics, but not overbearingly. Preferably there should be a
+        navigation drawer on the left.
+      </p>
+      <div className="grid-container">
+        {data.map((item) => (
+          <Card key={item.title} {...item} />
+        ))}
+      </div>
+    </>
+  );
 }
+
+export default Page;
